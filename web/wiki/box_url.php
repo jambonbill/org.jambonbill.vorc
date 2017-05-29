0930723e80d1ca36f4400206aef4c41fe30cc5d7
jambonbill <jambonbill@gmail.com>
2017-05-29 18:59:55 +0800

    +$URLS=$VORC->wikiUrls($ID);

--- web/wiki/box_url.php
+++ web/wiki/box_url.php
@@ -1,8 +1,30 @@
 <?php
+//URL's
+$URLS=$VORC->wikiUrls($ID);
+//print_r($URLS);
+$htm='<table class="table table-sm table-hover" style="cursor:pointer">';
+$htm.='<thead>';
+$htm.='<th>#</th>';
+$htm.='<th>URL</th>';
+//$htm.='<th>Comment</th>';
+$htm.='<th style="text-align:right"></th>';
+$htm.='</thead>';
+$htm.='<tbody>';
+foreach($URLS as $r){
+	$htm.='<tr data-id='.$r['wu_id'].'>';
+	$htm.='<td><i class="text-muted">'.$r['wu_id'];
+	$htm.='<td>'.$r['wu_url'];
+	$htm.='<td><i class="fa fa-times"></i>';//Delete
+	//$htm.='<td style="text-align:right">'.$r['wu_updated'];
+}
+$htm.='</tbody>';
+$htm.='</table>';
+
 $box=new LTE\Box;
 $box->id("boxUrl");
 $box->title("Url's");
 $box->icon("fa fa-list");
-$box->body("please wait");
-$box->footer('<a href=#btn id=btnAddUrl class="btn btn-default">Add url</a>');
+$box->body($htm);
+$box->footer('<a href=#btn id=btnAddUrl class="btn btn-secondary"><i class="fa fa-plus-circle"></i> Add url</a>');
+$box->loading(1);
 echo $box;
