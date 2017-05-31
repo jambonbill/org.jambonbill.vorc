fa94e03d6aa6736b11219694bb4e0fbe23ea12de
jambonbill <jambonbill@gmail.com>
2017-05-31 18:40:46 +0800

    case 'update':

--- web/wiki/ctrl.php
+++ web/wiki/ctrl.php
@@ -19,8 +19,31 @@ switch ($_POST['do']) {
         }
         exit(json_encode($dat));
 
+    case 'update':
+        $dat['POST']=$_POST;
+
+        $w_id=trim($_POST['w_id']);
+        $w_name=trim($_POST['w_name']);
+        $w_slug=trim($_POST['w_slug']);
+        $contents=trim($_POST['contents']);
+
+        $sql="UPDATE vorc.wiki SET w_name=".$VORC->db()->quote($w_name).", ";
+        $sql.=" w_slug=".$VORC->db()->quote($w_slug).", ";
+        $sql.=" contents=".$VORC->db()->quote($contents).", ";
+        $sql.=" w_updated=NOW(),";
+        $sql.=" w_updater=".$VORC->user_id();
+        $sql.=" WHERE w_id=$w_id LIMIT 1;";
+
+        $VORC->db()->query($sql) or die(print_r($VORC->db()->errorInfo(), true) . "<hr />$sql");
+
+        $dat['updated']=date('c');
+
+        exit(json_encode($dat));
+
+
     case 'addCategory':
         break;
+
 
     case 'addPlatform':
         break;
